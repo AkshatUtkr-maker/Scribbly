9f5184e3705f91407c4ab791bc0f3deb806ee83d
theCatasorasRex <utkarshakshat3@gmail.com>
2026-05-17 00:14:12 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Use the URL from your settings and the key you just sent me
-const SUPABASE_URL = 'https://wtinhuogjpquskzidwzp.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_jSJNS6w665HsE-jy8lPUhg_yYKqDhQA'
+const SUPABASE_URL = 'https://wtinhuogjpquskzidwzp.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_jSJNS6w665HsE-jy8lPUhg_yYKqDhQA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
